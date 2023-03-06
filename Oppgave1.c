799df9c4be350fc1fe2d1a6b9c83a51519595172
shakerhoudini <71225776+shakerhoudini@users.noreply.github.com>
2023-03-06 17:06:26 +0100

    Update Oppgave1.c

--- Oppgave1.c
+++ Oppgave1.c
@@ -45,7 +45,7 @@ void PWM_Init(){
 	TCA0.SINGLE.CTRLB =		TCA_SINGLE_CMP0EN_bm	|	TCA_SINGLE_WGMODE_DSBOTTOM_gc;
 	
 	TCA0.SINGLE.PERBUF	= TIMER;
-	TCA0.SINGLE.CMP0BUF = TIMER/2;//Duty Cycle, Using one from previous oppg
+	TCA0.SINGLE.CMP0BUF = TIMER/2;//Duty Cycle, Using one from previous task
 	
 	//Set Prescaler To 256 So values from oppg1 work	| enable Timer
 	TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV4_gc		| TCA_SINGLE_ENABLE_bm;
